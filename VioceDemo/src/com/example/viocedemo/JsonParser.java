dc8ca05be7cbb1297025afc04ca4de8fbe39359d
zxh <zxh@zxh-PC>
2016-07-11 20:30:59 +0800

    Frist test

---
+++ VioceDemo/src/com/example/viocedemo/JsonParser.java
@@ -0,0 +1,41 @@
+package com.example.viocedemo;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import android.text.TextUtils;
+
+/**
+ * 对云端返回的Json结果进行解析
+
+ */
+public class JsonParser {
+	
+	/**
+	 * 听写结果的Json格式解析
+	 * @param json
+	 * @return
+	 */
+	public static String parseIatResult(String json) {
+		if(TextUtils.isEmpty(json))
+			return "";
+		
+		StringBuffer ret = new StringBuffer();
+		try {
+			JSONTokener tokener = new JSONTokener(json);
+			JSONObject joResult = new JSONObject(tokener);
+
+			JSONArray words = joResult.getJSONArray("ws");
+			for (int i = 0; i < words.length(); i++) {
+				JSONArray items = words.getJSONObject(i).getJSONArray("cw");
+				JSONObject obj = items.getJSONObject(0);
+				ret.append(obj.getString("w"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		return ret.toString();
+	}
+	
+}
